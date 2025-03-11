0c14b091b071e8250dbae3a012115a82310340cd
pazko <parkcozmo@nate.com>
2025-03-11 15:26:45 +0900

    - add SnapKit, Then

--- xcode_issue/MainViewController.swift
+++ xcode_issue/MainViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 

715479cb09541ce73f671cb57abd48aa006bf8ae
pazko <parkcozmo@nate.com>
2025-03-11 14:51:12 +0900

    Initial Commit

---
+++ xcode_issue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcode_issue
+//
+//  Created by 박동혁 on 3/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ xcode_issueTests/xcode_issueTests.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_issueTests.swift
+//  xcode_issueTests
+//
+//  Created by 박동혁 on 3/11/25.
+//
+
+import Testing
+@testable import xcode_issue
+
+struct xcode_issueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
